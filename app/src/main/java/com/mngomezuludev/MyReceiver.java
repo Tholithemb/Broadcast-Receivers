c8dfb871cd743dc19a8a44eb4e9f97fac71ae7bd
Siyanda <siyandatholi09@gmail.com>
2021-02-17 11:11:47 +0200

    BroadCast Receiver

---
+++ app/src/main/java/com/mngomezuludev/MyReceiver.java
@@ -0,0 +1,70 @@
+package com.mngomezuludev;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "MyReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+       String actionString  = intent.getAction();
+
+       // Toast will pop-up when the users changes the time-zone
+        Toast.makeText(context, actionString, Toast.LENGTH_LONG).show();
+
+//         Get current time zone
+
+//        String timeZone = intent.getStringExtra("time-zone");
+//        Log.d(TAG, "onReceive: " +timeZone);
+
+//        boolean isOn = intent.getBooleanExtra("state",false);
+//        Log.d(TAG, "onReceive: Airplane mode is On "+ isOn);
+
+        PendingResult pendingResult = goAsync();
+
+        Log.d(TAG, "onReceive: Boot action");
+        new Task(pendingResult,intent).execute();
+
+
+    }
+
+    private static class  Task extends AsyncTask<Void,Void,Void>{
+
+        PendingResult pendingResult;
+        Intent intent;
+
+
+        public Task(PendingResult pendingResult, Intent intent) {
+            this.pendingResult = pendingResult;
+            this.intent = intent;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.d(TAG, "onPostExecute: Work Finished");
+            pendingResult.finish();
+        }
+    }
+
+}
